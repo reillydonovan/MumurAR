1b14e03a3109350f0ce09d56a45d64df02459bd4
reillydonovan <reillydonovan3@gmail.com>
2018-03-17 10:49:01 -0700

    added menu

---
+++ Assets/UIscripts/menuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuManager : MonoBehaviour {
+
+	public Menu CurrentMenu;
+
+	public void Start() {
+		ShowMenu (CurrentMenu);
+	}
+
+	public void ShowMenu(Menu menu) {
+		if (CurrentMenu != null)
+			CurrentMenu.IsOpen = false;
+
+		CurrentMenu = menu;
+		CurrentMenu.IsOpen = true;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# MumurAR
+# upm-package-template-3DLegacy
+Project Template for 3D scenes not using a SRP 
+
+This template uses Unity’s built in rendering pipeline. This template does not contain any example content.
+
+
+Package Name: com.unity.template.3dempty
+Package Version: 0.0.1
