df3a4fa76421c13d59e33af2ecea47d4aca9f0ca
reillydonovan <reillydonovan3@gmail.com>
2018-03-29 16:58:53 -0700

    gathering assets together for AR setup

---
+++ Assets/FadeSceneTransition/Demo/ButtonController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using TheNextFlow.UnityPlugins;
+
+public class ButtonController : MonoBehaviour {
+
+	public GameObject cube;
+
+	public void OnClickEvent() {
+		if (cube != null) {
+			FadeSceneTransition.FadeOut(1.0f, Color.black, () => {
+				cube.SetActive(!cube.activeSelf);
+				FadeSceneTransition.FadeIn(1.0f, Color.black, () => {});
+			});
+		} else if (Application.loadedLevelName == "SourceScene") {			
+			FadeSceneTransition.FadeAndLoadLevel("DestinationScene", 1.0f, Color.black, () => {
+				Debug.Log("Scene changed.");
+			});
+		} else {
+			FadeSceneTransition.FadeAndLoadLevel("SourceScene", 2.5f, Color.white, () => {
+				Debug.Log("Scene changed.");
+			});
+		}
+	}
+}
---
+++ Assets/FadeSceneTransition/Demo/Spin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public float speed = 100f;
+	public Vector3 direction = Vector3.up;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(direction, speed * Time.deltaTime);
+	}
+}
